ec827541a173f809921c44a2eeb5651f3c8645e3
Tomasz Ferens <tomaszferens@hotmail.com>
2025-05-12 23:22:51 +0200

    disable relative line numbers

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -21,3 +21,5 @@ vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#34462F" })
 vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#462F2F" })
 vim.api.nvim_set_hl(0, "DiffChange", { bg = "#2F4146" })
 vim.api.nvim_set_hl(0, "DiffText", { bg = "#463C2F" })
+
+vim.opt.relativenumber = false
