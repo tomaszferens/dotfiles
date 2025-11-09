3a970ba91c3898375cf75e509f25792939d50688
Tomasz Ferens <tomaszferens@hotmail.com>
2025-11-09 21:37:30 +0100

    conform

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,33 +1,21 @@
-local ts_format_eslint_prettier = {
-  "prettierd",
-}
-
-local per_project_formatters = {
-  miranda = {
-    typescript = ts_format_eslint_prettier,
-    typescriptreact = ts_format_eslint_prettier,
-  },
-  vinny = {
-    typescript = ts_format_eslint_prettier,
-    typescriptreact = ts_format_eslint_prettier,
-  },
-}
-
-function get_project_formatter()
-  local cwd = vim.fn.getcwd()
-  local project = cwd:match("([^/]+)$")
-
-  local config = per_project_formatters[project]
-  -- Default fallback for projects not in the list
-  return config or {
-    typescript = ts_format_eslint_prettier,
-    typescriptreact = ts_format_eslint_prettier,
-  }
-end
-
 return {
   "stevearc/conform.nvim",
   opts = {
-    formatters_by_ft = get_project_formatter(),
+    formatters_by_ft = {
+      javascript = { "prettierd" },
+      typescript = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      typescriptreact = { "prettierd" },
+      json = { "prettierd" },
+      jsonc = { "prettierd" },
+      css = { "prettierd" },
+      scss = { "prettierd" },
+      html = { "prettierd" },
+      yaml = { "prettierd" },
+      markdown = { "prettierd" },
+      graphql = { "prettierd" },
+      vue = { "prettierd" },
+      svelte = { "prettierd" },
+    },
   },
 }
